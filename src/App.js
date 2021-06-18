8cfcb7b476bfd75beb461d6a81fe9e4f6504e4f1
Nick Strong <nstrong92@yahoo.com>
2021-06-17 20:59:23 -0600

    added in all components and imported to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import NavBar from "./components/NavBar";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <Home />
+      <About />
+      <Resume />
+      <Portfolio />
+      <Resume />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
