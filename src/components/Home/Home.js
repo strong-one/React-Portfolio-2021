62472ecd65940dc59b62b53d195e97a8b4dc1cad
Nick Strong <nstrong92@yahoo.com>
2021-06-18 09:07:21 -0600

    styled home page, need to find all svg files for images

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,19 @@
+// importing css file to style locally
+import style from "./Home.module.css";
+// importing the filename of image from file path
+import computer from "../../img/computer.png";
+import code from "../../img/code.svg";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home page</h1>
+    <div class={style.Home}>
+      <div class={style.Container}>
+        <h1 class={style.Hello}>Nick Strong.</h1>
+        <h1>Aiming For progress and growth</h1>
+      </div>
+
+      <img class={style.code} src={code} alt="element"></img>
+      <img class={style.computer} src={computer} alt="computericon"></img>
     </div>
   );
 };
