75fb0edbe85f1c20cbf8e0ecd321545c4b9a2b79
Nick Strong <nstrong92@yahoo.com>
2021-06-21 17:15:42 -0600

    added more styling to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,25 @@
 // importing css file within folder within first div
 import style from "./NavBar.module.css";
-import Resume from "../Resume/Resume";
+// import Resume from "../Resume/Resume";
+// import { Navbar, Container, Nav } from "react-bootstrap";
 
 const NavBar = () => {
   return (
+    // <Navbar className={style.NavBar} bg="dark" variant="dark">
+    //   <Container className={style.Container}>
+    //     <Navbar.Brand href="#home">Nick Strong</Navbar.Brand>
+    //     <Nav className="me-auto">
+    //       <Nav.Link href="#home">Home</Nav.Link>
+    //       <Nav.Link href="/#about">About Me</Nav.Link>
+    //       <Nav.Link href="/#resume">Resume</Nav.Link>
+    //       <Nav.Link href="/#portfolio">Portfolio</Nav.Link>
+    //       <Nav.Link href="/#contact">Let's Connect</Nav.Link>
+    //     </Nav>
+    //   </Container>
+    // </Navbar>
+
     <div className={style.NavBar}>
+      <h1>Nick Strong</h1>
       <nav>
         <ul>
           <li>
@@ -13,7 +28,7 @@ const NavBar = () => {
           <li>
             <a href="/#about">About Me</a>
           </li>
-          <li onClick={Resume}>
+          <li>
             <a href="/#resume">Resume</a>
           </li>
           <li>
